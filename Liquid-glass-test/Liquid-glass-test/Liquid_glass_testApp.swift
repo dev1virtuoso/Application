7d75e178daa56720ae056bb27e27e1a9f7a2e6a7
dev1virtuoso <carson.developer1125@gmail.com>
2025-08-26 00:11:57 +0800

    updated dev1virtuoso and Liquid-glass

---
+++ Liquid-glass-test/Liquid-glass-test/Liquid_glass_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  Liquid_glass_testApp.swift
+//  Liquid-glass-test
+//
+//  Created by Carson Wu on 6/11/25.
+//
+
+import SwiftUI
+
+@main
+struct Liquid_glass_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Liquid-glass-test/Liquid-glass-testTests/Liquid_glass_testTests.swift
@@ -0,0 +1,16 @@
+//
+//  Liquid_glass_testTests.swift
+//  Liquid-glass-testTests
+//
+//  Created by Carson Wu on 6/11/25.
+//
+
+import Testing
+
+struct Liquid_glass_testTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
